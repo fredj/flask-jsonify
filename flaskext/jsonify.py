eb6b7009f518ccf21f02a89f05b8bbd9adc79a41
Frederic Junod <fredj@kirishima.wrk.lsn.camptocamp.com>
2010-06-08 11:25:16 +0200

    first commit

---
+++ flaskext/jsonify.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    flaskext.jsonify
+    ~~~~~~~~~~~~~~~~
+
+    Description of the module goes here...
+
+    :copyright: (c) 2010 by Frederic Junod.
+    :license: BSD, see LICENSE for more details.
+"""
---
+++ setup.py
@@ -0,0 +1,42 @@
+"""
+flask-jsonify
+-------------
+
+Description goes here...
+
+Links
+`````
+
+* `documentation <http://packages.python.org/flask-jsonify>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='flask-jsonify',
+    version='0.1',
+    url='<enter URL here>',
+    license='BSD',
+    author='Frederic Junod',
+    author_email='frederic.junod@gmail.com',
+    description='A small Flask decorator for returning json',
+    long_description=__doc__,
+    packages=['flaskext'],
+    namespace_packages=['flaskext'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
