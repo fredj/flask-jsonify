8d87d8fa5b90c7d33d3fe6974d4a85972ad85f13
Vasudeva <martriay@gmail.com>
2013-03-17 19:43:13 -0300

    Added functools.wraps, otherwise all the requests matched the last route

--- flaskext/jsonify.py
+++ flaskext/jsonify.py
@@ -5,8 +5,10 @@ except ImportError:
     from simplejson import dumps
 
 from flask import Response
+from functools import wraps
 
 def jsonify(f):
+    @wraps(f)
     def inner(*args, **kwargs):
         return Response(dumps(f(*args, **kwargs)), mimetype='application/json')
     return inner
